c911ebd6ad008f0cd7e6720cefac6d812b64b4ce
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2024-05-21 02:15:05 +0530

    added code

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -41,19 +41,25 @@ const Main = () => {
                         </div>
                     </>
                     :
-                    <div className="result">
+                    <div className='result'>
                         <div className="result-title">
                             <img src={assets.user_icon} alt=""/>
                             <p>{recentPrompt}</p>
                         </div>
                         <div className="result-data">
                             <img src={assets.gemini_icon} alt=""/>
-                            <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                            {
+                                loading ? <div className='loader'>
+                                        <hr/>
+                                        <hr/>
+                                        <hr/>
+                                    </div>
+                                    :
+                                    <p dangerouslySetInnerHTML={{__html: resultData}}></p>
+                            }
                         </div>
                     </div>
                 }
-
-
                 <div className="main-bottom">
                     <div className="search-box">
                         <input onChange={(e) => {
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,43 +1,77 @@
 import {createContext, useState} from "react";
-import run from "../config/gemini.js";
+import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
     const [input, setInput] = useState("");
-    const [recentPrompts, setRecentPrompts] = useState("");
-    const [prevPrompts, setPrevPrompts] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState([]);
     const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [resultData, setResultData] = useState({});
+    const [resultData, setResultData] = useState("");
 
-    const onSent = async () => {
+    const delayPara = (index, nextWord) => {
+        setTimeout(function () {
+            setResultData(prev => prev + nextWord)
+        }, 75 * index);
+    }
+
+    const newChat = () => {
+        setLoading(false);
+        setShowResult(false);
+    }
+
+    const onSent = async (prompt) => {
+
+
         setResultData("");
         setLoading(true);
         setShowResult(true);
-        setRecentPrompts(input);
-        const response = await run(input);
-        setResultData(response);
+        let response;
+        if (prompt !== undefined) {
+            response = await runChat(prompt);
+            setRecentPrompt(prompt);
+        } else {
+            setPrevPrompts(prev => [...prev, input]);
+            setRecentPrompt(input);
+            response = await runChat(input);
+        }
+
+        let responseArray = response.split("**");
+        let newResponse = "";
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responseArray[i];
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b>"
+            }
+
+        }
+        let newResponse2 = newResponse.split("*").join("</br>");
+        let newResponseArray = newResponse2.split(" ");
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i, nextWord + " ");
+        }
         setLoading(false);
         setInput("");
     }
-
-    // onSent("what is ReactJS")
 
     const contextValue = {
         prevPrompts,
         setPrevPrompts,
         onSent,
-        setRecentPrompts,
-        recentPrompts,
+        recentPrompt,
+        setRecentPrompt,
         showResult,
         loading,
         resultData,
         input,
         setInput,
+        newChat
     }
-
     return (
         <Context.Provider value={contextValue}>
             {props.children}
